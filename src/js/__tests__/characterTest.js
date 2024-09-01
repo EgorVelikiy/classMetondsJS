009e272102975bcfb8a7e3147f7040140f478b89
Egor <egor.velikiy.02@mail.ru>
2024-09-01 19:35:01 +0300

    initialCommit

--- src/js/__tests__/characterTest.js
+++ src/js/__tests__/characterTest.js
@@ -1,5 +1,5 @@
 import Character from '../character'
-
+import Bowman from '../bowman'
 test('Создание Character', () => {
 	const char = new Character('Егор', 'Undead');
 
@@ -22,4 +22,43 @@ test('Проверка имени', () => {
 
 test('Проверка типа', () => {
 	expect(() => {new Character('Егор', 'Student')}).toThrow();
+});
+
+test('Получение нового уровня персонажа hp > 0', () => {
+	const char = new Bowman('Егор');
+	char.levelUp();
+
+	const obj = {
+		name: 'Егор',
+		type: 'Bowman',
+		health: 100,
+		level: 2,
+		attack: 30,
+		defence: 30,
+	}
+
+	expect(char).toEqual(obj);
+});
+
+test('Получение нового уровня персонажа hp < 0', () => {
+	const char = new Bowman('Егор');
+	char.health = 0;
+
+	expect(() => char.levelUp()).toThrow();
+});
+
+
+test('Получение урона персонажем hp > 0', () => {
+	const char = new Bowman('Егор');
+	char.damage(40)
+
+
+	expect(char.health).toBe(70);
+});
+
+test('Получение урона персонажем hp < 0', () => {
+	const char = new Bowman('Егор');
+	char.health = 0
+
+	expect(() => char.damage(40)).toThrow();
 });
--- src/js/character.js
+++ src/js/character.js
@@ -17,4 +17,23 @@ export default class Character {
 		this.attack = undefined;
 		this.defence = undefined;
 	}
+
+	levelUp() {
+		if (this.health > 0) {
+			this.level += 1;
+			this.attack *= 1.2;
+			this.defence *= 1.2;
+			this.health = 100
+		} else {
+			throw new Error('нельзя повысить левел умершего')
+		}
+	}
+
+	damage(points) {
+		if (this.health !== 0) {
+			this.health -= points * (1 - (this.defence / 100))
+		} else {
+			throw new Error('Персонаж уже мертв')
+		}
+	}
 }
